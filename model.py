72625eef1d28a938c1009dbe27de8617300ca3ea
Towards EasilyLearn <jmi.shahid@gmail.com>
2021-01-01 01:43:20 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,55 @@
+
+### Import Libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import pickle
+
+warnings.filterwarnings('ignore')
+
+### Import Datset
+df = pd.read_csv("data_breast-cancer-wiscons.csv")
+
+
+
+### Splitting Data
+
+X = df[['texture_mean', 'area_mean', 'concavity_mean', 'area_se', 'concavity_se','fractal_dimension_se','smoothness_worst','concavity_worst', 'symmetry_worst','fractal_dimension_worst']]
+y = df['diagnosis']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=0)
+
+#### Data Preprocessing
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+x_train = scaler.fit_transform(X_train)
+x_test = scaler.transform(X_test)
+
+
+##
+from sklearn.linear_model import LogisticRegression
+clf_lr = LogisticRegression()
+clf_lr.fit(x_train, y_train)
+predictions = clf_lr.predict(x_test)
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+print("Confusion Matrix : \n\n" , confusion_matrix(predictions,y_test))
+
+print("Classification Report : \n\n" , classification_report(predictions,y_test),"\n")
+
+
+pickle.dump(clf_lr, open('model.pkl', 'wb'))
+pickle.dump(scaler, open('scaler.pkl', 'wb'))
+
+model = pickle.load(open('model.pkl', 'rb'))
+print(model)
